5a44f3e22545a84d5d70cf938f49ee2cdddc4605
sidore_m <marc@sidorenko.fr>
2021-02-23 23:55:54 +0100

    Add algov1, add score

--- training/basic.py
+++ training/basic.py
@@ -1,5 +1,22 @@
-from .model import Pizza
+from model import Pizza
 
 
-def score_2_pizzas(a: Pizza, b: Pizza) :
-    return a.nb_in + a.nb_in - set(a.list_in).update(b.list_in)
+def score_2_pizzas(a: Pizza, b: Pizza):
+    inset = set(a.list_in)
+    inset.update(b.list_in)
+    return a.nb_in + b.nb_in - inset
+
+
+def score_3_pizzas(a: Pizza, b: Pizza, c: Pizza):
+    inset = set(a.list_in)
+    inset.update(b.list_in)
+    inset.update(c.list_in)
+    return a.nb_in + b.nb_in + c.nb_in - inset
+
+
+def score_4_pizzas(a: Pizza, b: Pizza, c: Pizza, d: Pizza):
+    inset = set(a.list_in)
+    inset.update(b.list_in)
+    inset.update(c.list_in)
+    inset.update(d.list_in)
+    return a.nb_in + b.nb_in + c.nb_in + d.nb_in - inset
