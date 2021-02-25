b4fdb837cf493a7c6a1137cd6ab873a1ed5ae753
sidore_m <marc@sidorenko.fr>
2021-02-25 19:29:12 +0100

    fix

--- model.py
+++ model.py
@@ -1,22 +1,26 @@
 
 class Street:
-    def __init__(self, id, len: int, interA: int, interB: int):
+    def __init__(self, street_id, len: int, inter_a: int, inter_b: int):
         self.id = id
         self.len = len
-        self.interA = interA
-        self.interB = interB
+        self.interA = inter_a
+        self.interB = inter_b
+        self.cars_list = []
 
 
 class Intersection:
-    def __init__(self, streetsIn : [], streetsOut: []):
-        self.streetsIn = streetsIn
-        self.streetsOut = streetsOut
+    def __init__(self, inter_id, streets_in: [], streets_out: []):
+        self.streetsIn = streets_in
+        self.streetsOut = streets_out
 
 
 class Car:
-    def __init__(self, id, minTimeLeft: int, stuck: bool, path: []):
-        self.id = id
-        self.minTimeLeft = minTimeLeft
+    def __init__(self, car_id, min_time_left: int, stuck: bool, path: []):
+        self.id = car_id
+        self.minTimeLeft = min_time_left
         self.stuck = stuck
         self.path = path
+
+class TrafficLight:
+    def __init__(self, ):
 
